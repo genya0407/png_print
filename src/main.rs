7977f00f2f1a7ec44dde099302fcd5016285a2d5
Yusuke Sangenya <longinus.eva@gmail.com>
2017-10-08 19:14:47 +0900

    define trait

--- src/main.rs
+++ src/main.rs
@@ -8,41 +8,49 @@ use std::str;
 fn main() {
     let filename = args().nth(1).unwrap();
     let bytes = readfile(&filename).unwrap();
-    let chunks = get_chunks(bytes);
+    let chunks = parse_to_chunks(bytes);
     for c in chunks.unwrap() {
-        println!("{}", c.chunk_type_string());
+        println!("{}", c.chunk_type());
     }
 }
 
-#[derive(Debug)]
-struct Chunk {
-    pub chunk_length: u32,
-    pub chunk_type: [u8; 4],
-    pub chunk_data: Vec<u8>,
-    pub chunk_crc: [u8; 4],
+trait IChunk {
+    //fn chunk_length(&self) -> u32;
+    fn chunk_type(&self) -> String;
+    //fn chunk_data(&self) -> &Vec<u8>;
+    //fn chunk_crc(&self) -> u32;
 }
 
-impl Chunk {
-    pub fn chunk_type_string(&self) -> String {
-        format!("{}{}{}{}", self.chunk_type[0] as char, self.chunk_type[1] as char, self.chunk_type[2] as char, self.chunk_type[3] as char)
+#[derive(Debug)]
+struct GeneralChunk {
+    chunk_length: u32,
+    chunk_type: String,
+    chunk_data: Vec<u8>,
+    chunk_crc: [u8; 4],
+}
+
+struct 
+
+impl IChunk for GeneralChunk {
+    fn chunk_type(&self) -> String {
+        self.chunk_type.clone()
     }
 }
 
 const PNG_HEADER_SIZE: usize = 8;
-fn get_chunks(bytes: Vec<u8>) -> Result<Vec<Chunk>, Box<Error>> {
-    let mut chunks: Vec<Chunk> = Vec::new();
+fn parse_to_chunks(bytes: Vec<u8>) -> Result<Vec<Box<IChunk>>, Box<Error>> {
+    let mut chunks: Vec<Box<IChunk>> = Vec::new();
     let mut iter = bytes.into_iter().skip(PNG_HEADER_SIZE);
     loop {
         let chunk_length = (iter.next().unwrap() as u32) * (2u32.pow(24)) +
             (iter.next().unwrap() as u32) * (2u32.pow(16)) +
             (iter.next().unwrap() as u32) * (2u32.pow(8)) +
             (iter.next().unwrap() as u32);
-        let chunk_type = [
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap()
-        ];
+        let chunk_type = format!("{}{}{}{}",
+                                 iter.next().unwrap() as char,
+                                 iter.next().unwrap() as char,
+                                 iter.next().unwrap() as char,
+                                 iter.next().unwrap() as char);
         let mut chunk_data = Vec::new();
         for _ in 0..chunk_length {
             chunk_data.push(iter.next().unwrap());
@@ -54,17 +62,17 @@ fn get_chunks(bytes: Vec<u8>) -> Result<Vec<Chunk>, Box<Error>> {
             iter.next().unwrap()
         ];
 
-        let chunk = Chunk {
+        let chunk = GeneralChunk {
             chunk_length: chunk_length,
             chunk_type: chunk_type,
             chunk_data: chunk_data,
             chunk_crc: chunk_crc,
         };
-        if chunk.chunk_type_string() == "IEND".to_string() {
-            chunks.push(chunk);
+        if chunk.chunk_type() == "IEND".to_string() {
+            chunks.push(Box::new(chunk));
             break;
         } else {
-            chunks.push(chunk);
+            chunks.push(Box::new(chunk));
         }
     }
     Ok(chunks)
