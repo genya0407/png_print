1185b1e58be55f8e87762a404732411e47ac23fe
Yusuke Sangenya <longinus.eva@gmail.com>
2017-10-10 11:25:28 +0900

    to_string

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn parse_to_chunks(bytes: Vec<u8>) -> Result<Vec<GeneralChunk>, Box<Error>> {
     let mut base_index: usize = PNG_HEADER_SIZE;
     loop {
         let chunk_length     = read_bytes(&bytes, base_index, 4).read_u32::<BigEndian>()? as usize;
-        let chunk_type       = String::from(str::from_utf8(read_bytes(&bytes, base_index+4, 4))?);
+        let chunk_type       = str::from_utf8(read_bytes(&bytes, base_index+4, 4))?.to_string();
         let chunk_data_slice = read_bytes(&bytes, base_index+8, chunk_length);
         let chunk_crc        = read_bytes(&bytes, base_index+chunk_length, 4).read_u32::<BigEndian>()?;
 
