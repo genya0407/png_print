c7683e74db17276da7b3bb14c5f21dd7f48bf762
Yusuke Sangenya <longinus.eva@gmail.com>
2017-10-10 11:12:46 +0900

    remove unwrap

--- src/main.rs
+++ src/main.rs
@@ -8,67 +8,30 @@ use std::env::args;
 use std::str;
 use byteorder::{BigEndian, ReadBytesExt};
 
+mod model;
+use model::*;
+
 fn main() {
     let filename = args().nth(1).unwrap();
     let bytes = readfile(&filename).unwrap();
     let chunks = parse_to_chunks(bytes);
-}
-
-struct PNG {
-    ihdr: IHDR,
-    idats: Vec<IDAT>,
-    iend: IEND,
-    others: Vec<GeneralChunk>
-}
-
-struct IDAT {}
-struct IEND {}
-
-#[derive(Debug)]
-struct GeneralChunk {
-    chunk_length: usize,
-    chunk_type: String,
-    chunk_data: Vec<u8>,
-    chunk_crc: u32,
-}
-
-#[derive(Debug)]
-struct IHDR {
-    chunk_length:       usize,
-    chunk_crc:          u32,
-    width:              u32,
-    height:             u32,
-    bit_depth:          u8,
-    color_type:         u8,
-    compression_method: u8,
-    filter_method:      u8,
-    interlace_method:   u8,
-}
-impl IHDR {
-    pub fn new(chunk_length: usize, chunk_data: &[u8], chunk_crc: u32) -> Self {
-        Self {
-            chunk_length: chunk_length,
-            chunk_crc: chunk_crc,
-            width: (&chunk_data[0..4]).read_u32::<BigEndian>().unwrap(),
-            height: (&chunk_data[4..8]).read_u32::<BigEndian>().unwrap(),
-            bit_depth: chunk_data[8],
-            color_type: chunk_data[9],
-            compression_method: chunk_data[10],
-            filter_method: chunk_data[11],
-            interlace_method: chunk_data[12],
-        }
+    for chunk in chunks.unwrap().into_iter() {
+        println!("{:?}", chunk.chunk_type);
     }
 }
+
+//fn parse_to_png(chunks: Vec<GeneralChunk>) -> Result<PNG, Box<Error>> {
+//}
 
 const PNG_HEADER_SIZE: usize = 8;
 fn parse_to_chunks(bytes: Vec<u8>) -> Result<Vec<GeneralChunk>, Box<Error>> {
     let mut chunks = Vec::new();
     let mut base_index: usize = PNG_HEADER_SIZE;
     loop {
-        let chunk_length = (&bytes[base_index..base_index+4]).read_u32::<BigEndian>().unwrap() as usize;
-        let chunk_type = String::from(str::from_utf8(&bytes[base_index+4..base_index+8]).unwrap());
+        let chunk_length = (&bytes[base_index..base_index+4]).read_u32::<BigEndian>()? as usize;
+        let chunk_type = String::from(str::from_utf8(&bytes[base_index+4..base_index+8])?);
         let chunk_data_slice = &bytes[(base_index+8)..(base_index+8+chunk_length)];
-        let chunk_crc = (&bytes[(base_index+chunk_length)..(base_index+chunk_length+4)]).read_u32::<BigEndian>().unwrap();
+        let chunk_crc = (&bytes[(base_index+chunk_length)..(base_index+chunk_length+4)]).read_u32::<BigEndian>()?;
 
         let mut chunk_data: Vec<u8> = Vec::new();
         chunk_data.extend_from_slice(chunk_data_slice);
---
+++ src/model.rs
@@ -0,0 +1,49 @@
+use byteorder::{BigEndian, ReadBytesExt};
+
+pub struct PNG {
+    pub ihdr: IHDR,
+    pub plte: PLTE,
+    pub idats: Vec<IDAT>,
+    pub iend: IEND,
+    pub others: Vec<GeneralChunk>
+}
+
+pub struct IDAT {}
+pub struct IEND {}
+pub struct PLTE {}
+
+#[derive(Debug)]
+pub struct GeneralChunk {
+    pub chunk_length: usize,
+    pub chunk_type: String,
+    pub chunk_data: Vec<u8>,
+    pub chunk_crc: u32,
+}
+
+#[derive(Debug)]
+pub struct IHDR {
+    pub chunk_length:       usize,
+    pub chunk_crc:          u32,
+    pub width:              u32,
+    pub height:             u32,
+    pub bit_depth:          u8,
+    pub color_type:         u8,
+    pub compression_method: u8,
+    pub filter_method:      u8,
+    pub interlace_method:   u8,
+}
+impl IHDR {
+    pub fn new(chunk_length: usize, chunk_data: &[u8], chunk_crc: u32) -> Self {
+        Self {
+            chunk_length: chunk_length,
+            chunk_crc: chunk_crc,
+            width: (&chunk_data[0..4]).read_u32::<BigEndian>().unwrap(),
+            height: (&chunk_data[4..8]).read_u32::<BigEndian>().unwrap(),
+            bit_depth: chunk_data[8],
+            color_type: chunk_data[9],
+            compression_method: chunk_data[10],
+            filter_method: chunk_data[11],
+            interlace_method: chunk_data[12],
+        }
+    }
+}
