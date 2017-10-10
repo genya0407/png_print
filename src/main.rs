4cd1f62c9a0033228e5287f1562fe4eac557ccfe
Yusuke Sangenya <longinus.eva@gmail.com>
2017-10-10 11:22:32 +0900

    read_bytes function

--- src/main.rs
+++ src/main.rs
@@ -28,17 +28,15 @@ fn parse_to_chunks(bytes: Vec<u8>) -> Result<Vec<GeneralChunk>, Box<Error>> {
     let mut chunks = Vec::new();
     let mut base_index: usize = PNG_HEADER_SIZE;
     loop {
-        let chunk_length = (&bytes[base_index..base_index+4]).read_u32::<BigEndian>()? as usize;
-        let chunk_type = String::from(str::from_utf8(&bytes[base_index+4..base_index+8])?);
-        let chunk_data_slice = &bytes[(base_index+8)..(base_index+8+chunk_length)];
-        let chunk_crc = (&bytes[(base_index+chunk_length)..(base_index+chunk_length+4)]).read_u32::<BigEndian>()?;
+        let chunk_length     = read_bytes(&bytes, base_index, 4).read_u32::<BigEndian>()? as usize;
+        let chunk_type       = String::from(str::from_utf8(read_bytes(&bytes, base_index+4, 4))?);
+        let chunk_data_slice = read_bytes(&bytes, base_index+8, chunk_length);
+        let chunk_crc        = read_bytes(&bytes, base_index+chunk_length, 4).read_u32::<BigEndian>()?;
 
-        let mut chunk_data: Vec<u8> = Vec::new();
-        chunk_data.extend_from_slice(chunk_data_slice);
         let chunk = GeneralChunk {
             chunk_length: chunk_length,
             chunk_type: chunk_type,
-            chunk_data: chunk_data,
+            chunk_data: chunk_data_slice.to_vec(),
             chunk_crc: chunk_crc,
         };
         base_index += 12 + chunk_length;
@@ -51,6 +49,10 @@ fn parse_to_chunks(bytes: Vec<u8>) -> Result<Vec<GeneralChunk>, Box<Error>> {
         }
     }
     Ok(chunks)
+}
+
+fn read_bytes(bytes: &Vec<u8>, from: usize, len: usize) -> &[u8] {
+    &bytes[from..(from+len)]
 }
 
 fn readfile(filename: &str) -> Result<Vec<u8>, Box<Error>> {
